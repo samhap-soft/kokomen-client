5a02cb14ed726ba2b137c9e2c405e5cef0158d72
Minhyung Cho <dup33333@hufs.ac.kr>
2025-05-27 02:59:39 +0900

    fix : 메인페이지에 Button 컴포넌트 제거 및 tsconfig 설정 보강

--- apps/client/src/pages/index.tsx
+++ apps/client/src/pages/index.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@kokomen/ui";
 import { Geist, Geist_Mono } from "next/font/google";
 import Image from "next/image";
 
@@ -17,7 +16,6 @@ export default function Home() {
     <div
       className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
-      <Button />
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
