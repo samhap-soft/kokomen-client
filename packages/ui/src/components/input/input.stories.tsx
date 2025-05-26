3aeb7d73e7bfc00eac4910cf9b0e0b85d03ac5e2
Minhyung Cho <dup33333@hufs.ac.kr>
2025-05-27 02:41:15 +0900

    feat : Input 컴포넌트 및 스토리 추가

---
+++ packages/ui/src/components/input/input.stories.tsx
@@ -0,0 +1,75 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+import { Input } from "./input.tsx";
+
+const meta: Meta<typeof Input> = {
+  title: "Common/Input",
+  component: Input,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  args: {
+    onClick: fn(),
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Input>;
+
+export const Primary: Story = {
+  args: {
+    type: "text",
+    variant: "default", // 'primary'가 ButtonProps.variant의 유효한 값이라고 가정
+    size: "lg", // 'medium'이 ButtonProps.size의 유효한 값이라고 가정
+  },
+};
+
+export const Number: Story = {
+  args: {
+    type: "number",
+    variant: "default", // 'primary'가 ButtonProps.variant의 유효한 값이라고 가정
+    size: "lg", // 'medium'이 ButtonProps.size의 유효한 값이라고 가정
+  },
+};
+
+export const Time: Story = {
+  args: {
+    type: "time",
+    variant: "default", // 'primary'가 ButtonProps.variant의 유효한 값이라고 가정
+    size: "lg", // 'medium'이 ButtonProps.size의 유효한 값이라고 가정
+  },
+};
+
+export const Date: Story = {
+  args: {
+    type: "date",
+    variant: "default", // 'primary'가 ButtonProps.variant의 유효한 값이라고 가정
+    size: "lg", // 'medium'이 ButtonProps.size의 유효한 값이라고 가정
+  },
+};
+
+export const Password: Story = {
+  args: {
+    type: "password",
+    variant: "default", // 'primary'가 ButtonProps.variant의 유효한 값이라고 가정
+    size: "lg", // 'medium'이 ButtonProps.size의 유효한 값이라고 가정
+  },
+};
+
+export const Radio: Story = {
+  args: {
+    type: "radio",
+    variant: "default", // 'primary'가 ButtonProps.variant의 유효한 값이라고 가정
+    size: "lg", // 'medium'이 ButtonProps.size의 유효한 값이라고 가정
+  },
+};
+
+export const Checkbox: Story = {
+  args: {
+    type: "checkbox",
+    variant: "default", // 'primary'가 ButtonProps.variant의 유효한 값이라고 가정
+    size: "lg", // 'medium'이 ButtonProps.size의 유효한 값이라고 가정
+  },
+};
---
+++ packages/ui/src/components/input/input.tsx
@@ -0,0 +1,59 @@
+import { cva, VariantProps } from "class-variance-authority";
+import React from "react";
+
+type InputVariantProps = VariantProps<typeof inputVariants>;
+const inputVariants = cva(
+  "flex items-center rounded-md p-2 outline-border-input bg-gray- outline-2 hover:outline-primary-hover transition-all focus:outline-border-input-focus",
+  {
+    variants: {
+      variant: {
+        default: "border-gray-300",
+        outline: "border-borderinput",
+        red: "border-red-600",
+      },
+      size: {
+        default: "text-base",
+        sm: "text-sm",
+        lg: "text-lg",
+        xl: "text-xl",
+        "2xl": "text-2xl",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+interface InputProps
+  extends Omit<
+      React.InputHTMLAttributes<HTMLInputElement>,
+      "size" | "children" | "dangerouslySetInnerHTML"
+    >,
+    InputVariantProps {
+  ref?: React.Ref<HTMLInputElement>;
+  children?: React.ReactNode;
+  name: string;
+}
+export const Input = ({
+  variant,
+  type,
+  className,
+  size,
+  ref,
+  name,
+  ...props
+}: InputProps) => (
+  <label htmlFor={name}>
+    <input
+      dangerouslySetInnerHTML={undefined}
+      ref={ref}
+      type={type}
+      className={inputVariants({ variant, size })}
+      id={name}
+      {...props}
+    />
+    {props.children}
+  </label>
+);
