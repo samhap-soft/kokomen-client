e3df21b7496a27ec9a8bfb0a7e0446d33a85616d
Minhyung Cho <dup33333@hufs.ac.kr>
2025-05-28 11:54:18 +0900

    feat : Textarea 컴포넌트 추가 및 자동 높이 조정 기능 기본으로 설정

---
+++ packages/ui/src/components/textarea/textarea.tsx
@@ -0,0 +1,76 @@
+import { cn } from "#utils/index.ts";
+import { cva, VariantProps } from "class-variance-authority";
+import React, { RefObject, useCallback, useEffect, useRef } from "react";
+
+type TextareaVariantProps = VariantProps<typeof textareaVariants>;
+const textareaVariants = cva("flex items-center rounded-xl p-2 s resize-none", {
+  variants: {
+    variant: {
+      default: "border-border-input",
+      red: "border-red-600",
+    },
+    border: {
+      default:
+        "border hover:border-primary-hover transition-all focus:border-primary",
+      none: "border-none focus:outline-none",
+    },
+    size: {
+      default: "text-base",
+      sm: "text-sm",
+      lg: "text-lg",
+      xl: "text-xl",
+      "2xl": "text-2xl",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
+});
+
+interface TextareaProps
+  extends Omit<
+      React.TextareaHTMLAttributes<HTMLTextAreaElement>,
+      "size" | "children" | "dangerouslySetInnerHTML"
+    >,
+    TextareaVariantProps {
+  ref?: RefObject<HTMLTextAreaElement | null>;
+  autoAdjust?: boolean;
+  name: string;
+}
+
+export const Textarea = ({
+  className,
+  variant,
+  size,
+  border,
+  name,
+  ref,
+  autoAdjust = false,
+  ...props
+}: TextareaProps) => {
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+  const currentRef = ref || textareaRef;
+  useEffect(() => {
+    if (
+      autoAdjust &&
+      currentRef &&
+      currentRef.current &&
+      props.value !== undefined
+    ) {
+      const textarea = currentRef.current;
+      textarea.style.height = "auto";
+      textarea.style.height = `${textarea.scrollHeight > 400 ? 400 : textarea.scrollHeight}px`;
+    }
+  }, [props.value, autoAdjust, ref]);
+
+  return (
+    <textarea
+      className={cn(textareaVariants({ variant, size, border }), className)}
+      ref={ref}
+      name="textarea"
+      placeholder="Type your text here..."
+      {...props}
+    />
+  );
+};
