880898f2f22a3a01b7587245dab7ad2c896b61c9
Minhyung Cho <dup33333@hufs.ac.kr>
2025-05-29 13:22:58 +0900

    feat: 인터뷰 답변 제출 API 함수 추가

---
+++ apps/client/src/domains/interview/api/interviewAnswer.ts
@@ -0,0 +1,36 @@
+import { interviewApiInstance } from "@/domains/interview/api";
+import { AxiosResponse } from "axios";
+
+interface InterviewAnswerApiRequest {
+  answer: string;
+}
+
+interface IntercviewAnswerApiResponse {
+  question_id: number;
+  question: string;
+  is_root: boolean;
+}
+
+export async function submitInterviewAnswer({
+  interview_id,
+  question_id,
+  answer,
+}: {
+  interview_id: string;
+  question_id: string;
+  answer: string;
+}) {
+  return interviewApiInstance.post<
+    IntercviewAnswerApiResponse,
+    AxiosResponse<IntercviewAnswerApiResponse>,
+    InterviewAnswerApiRequest
+  >(
+    `/interviews/${interview_id}/questions/${question_id}/answers`,
+    {
+      answer: answer,
+    },
+    {
+      timeout: 10000,
+    }
+  );
+}
