9932a061db7772ef6374c19754ebc6d5e291af85
Minhyung Cho <dup33333@hufs.ac.kr>
2025-05-29 20:59:55 +0900

    feat : 새로운 면접 시작 api 추가 및 api 인스턴스 추가

---
+++ apps/client/src/domains/interview/api/index.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosResponse } from "axios";
+
+export const interviewApiInstance = axios.create({
+  baseURL: "http://13.124.13.60/api/v1",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+interface NewInterviewResponse {
+  interview_id: string;
+  question_id: string;
+  root_question: string;
+}
+
+export const startNewInterview = async (): Promise<
+  AxiosResponse<NewInterviewResponse>
+> => {
+  return interviewApiInstance.post("/interviews", {
+    categories: ["OPERATING_SYSTEM"],
+  });
+};
