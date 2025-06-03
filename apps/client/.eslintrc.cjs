9e59f61094e636ffd666f3851c454eda5d9d3321
Minhyung Cho <dup33333@hufs.ac.kr>
2025-06-03 16:50:11 +0900

    setting : 클라이언트 패키지 린팅 설정

---
+++ apps/client/.eslintrc.cjs
@@ -0,0 +1,12 @@
+require("@kokomen/eslint-config/patch");
+
+module.exports = {
+  parserOptions: {
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+    ecmaVersion: 2020,
+  },
+  extends: ["@kokomen/eslint-config", "@kokomen/eslint-config/mixins/next"],
+  rules: {},
+};
