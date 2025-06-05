347cb79eb50a7ff8811c9034702bf0999b44feb4
Minhyung Cho <dup33333@hufs.ac.kr>
2025-06-05 22:02:38 +0900

    setting : 린팅 설정 변경 및 Parser 설정 제거

--- packages/eslint-config/mixins/next.js
+++ packages/eslint-config/mixins/next.js
@@ -5,15 +5,13 @@ module.exports = {
     "plugin:jsx-a11y/recommended",
     "plugin:react-hooks/recommended",
     "plugin:@next/next/recommended",
-    "plugin:@typescript-eslint/recommended",
   ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "./tsconfig.json",
+  settings: {
+    react: {
+      version: "detect",
+    },
   },
-  plugins: ["@typescript-eslint", "react", "jsx-a11y"],
   rules: {
     "react/react-in-jsx-scope": "off",
-    "@typescript-eslint/no-unused-vars": "warn",
   },
 };
