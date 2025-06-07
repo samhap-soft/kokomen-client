d331725519ecb9de49347b25bb2bd77dcc4ffd7b
Minhyung Cho <dup33333@hufs.ac.kr>
2025-06-07 21:08:38 +0900

    feat : queryProvider 설정

--- apps/client/src/pages/_app.tsx
+++ apps/client/src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import { JSX } from "react";
+const queryClient: QueryClient = new QueryClient();
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
