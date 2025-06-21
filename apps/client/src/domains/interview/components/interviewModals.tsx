7b20ad511b94cfd661a389a48ba9c05b55cfd7d6
Minhyung Cho <dup33333@hufs.ac.kr>
2025-06-22 01:39:43 +0900

    fix: 인터뷰 시작 모달의 스타일 및 버튼 속성 수정

--- apps/client/src/domains/interview/components/interviewModals.tsx
+++ apps/client/src/domains/interview/components/interviewModals.tsx
@@ -46,22 +46,23 @@ function StartNewInterviewModal({
   };
   return (
     <Modal visible={interviewModal}>
-      <div className="flex flex-col items-center justify-center p-8 w-1/2 min-w-[450px] bg-background-base rounded-xl">
+      <div className="flex flex-col items-center justify-center p-8 w-1/2 min-w-[450px] bg-bg-base rounded-xl">
         <h2 className="text-2xl font-bold mb-4">인터뷰 시작하기</h2>
         <p className="text-lg">인터뷰를 시작합니다. 준비 되셨나요?</p>
         <div className="mt-8 flex gap-4 w-full">
           <Button
             className="w-full"
-            size={"lg"}
-            variant={"red"}
+            size={"default"}
+            variant={"primary"}
+            danger
             onClick={() => router.back()}
           >
             나가기
           </Button>
           <Button
             className="w-full"
-            variant={"default"}
-            size={"lg"}
+            variant={"primary"}
+            size={"default"}
             onClick={() => startup()}
           >
             시작하기
