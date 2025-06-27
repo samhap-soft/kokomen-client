6f948a8cdbbf771390b65f0dfcc3d8ae9a093964
Minhyung Cho <dup33333@hufs.ac.kr>
2025-06-27 15:44:20 +0900

    feat: PrevQuestionAndAnswers 및 PrevInterview 타입 추가

--- apps/client/src/domains/interview/types/index.ts
+++ apps/client/src/domains/interview/types/index.ts
@@ -1,3 +1,20 @@
 type InterviewStatus = "standby" | "thinking" | "question" | "finished";
 
-export type { InterviewStatus };
+type QuestionAndAnswer = {
+  question_id: number;
+  answer_id: number;
+  question: string;
+  answer: string;
+};
+type PrevQuestionAndAnswers = Array<QuestionAndAnswer>;
+
+type PrevInterview = {
+  interview_state: "FINISHED" | "IN_PROGRESS";
+  prev_question_and_answers: PrevQuestionAndAnswers;
+  cur_question_id: number;
+  question: string;
+  cur_question_count: number;
+  max_question_count: number;
+};
+
+export type { InterviewStatus, PrevQuestionAndAnswers, PrevInterview };
