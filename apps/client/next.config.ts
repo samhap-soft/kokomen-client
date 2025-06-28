03abb14b0eaa6c581236858997f0443e72f5d3ec
Minhyung Cho <126141140+alsgud8311@users.noreply.github.com>
2025-06-28 19:17:21 +0900

    [Fix/optimization] - 빌드 타임 및 메모리 최적화를 위한 작업 (#18)
    * fix : GitHub Actions 워크플로우에서 Lighthouse CI가 PR에서만 댓글이 추가되도록 수정
    
    * feat: 빌드 분석을 위한 @next/bundle-analyzer 의존성 추가 및 스크립트 추가
    
    * feat: Lighthouse CI 워크플로우 이름 변경 및 빌드 시간 측정 기능 추가
    
    * feat: Lighthouse 및 빌드 시간 체크를 위한 GitHub Actions 워크플로우 로직 추가
    
    * feat : yarn pnp 환경에서 path resolve를 하기 위한 라이브러리 설치
    
    * fix: AI 인터뷰어 인터페이스, 면접 배경 컴포넌트 분리 및 dynamic import를 활용하여 페이지 사이즈 최소화
    
    * refactor: AI 인터뷰어 컴포넌트를 동적 임포트로 변경하고, 면접 배경을 새로운 인터페이스로 통합하여 레이아웃 개선
    
    * feat: Next.js 설정 파일에 Webpack 구성 추가 및 ESLint, TypeScript 설정 업데이트

--- apps/client/next.config.ts
+++ apps/client/next.config.ts
@@ -1,12 +1,49 @@
 import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
+import bundleAnalyzer from "@next/bundle-analyzer";
+import path from "path";
+import PnpWebpackPlugin from "pnp-webpack-plugin";
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   transpilePackages: ["@kokomen/ui"],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  productionBrowserSourceMaps: false,
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      config.devtool = false;
+      config.optimization.splitChunks = {
+        chunks: "all",
+        minChunks: 2,
+        maxInitialRequests: 10,
+      };
+    }
+    config.resolve.plugins = config.resolve.plugins || [];
+    config.resolve.plugins.push(PnpWebpackPlugin);
+
+    config.resolveLoader = {
+      ...config.resolveLoader,
+      plugins: [PnpWebpackPlugin.moduleLoader(import.meta.url)],
+    };
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve("./src"),
+      "@kokomen/ui": path.resolve("../../packages/ui/src"),
+    };
+    return config;
+  },
 };
 
-export default withSentryConfig(nextConfig, {
+export default withSentryConfig(withBundleAnalyzer(nextConfig), {
   // For all available options, see:
   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
@@ -20,7 +57,7 @@ export default withSentryConfig(nextConfig, {
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
   // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
+  widenClientFileUpload: false,
 
   // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
   // This can increase your server load as well as your hosting bill.
