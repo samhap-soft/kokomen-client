eafb5b0efcf7c1d86d4a5e3c96fb16793e8d59e1
Minhyung Cho <dup33333@hufs.ac.kr>
2025-07-05 22:14:22 +0900

    refactor : 레이아웃쪽에서 Error boundary로 unexpected Error를 잡는 동시에 sentry에 전송

--- apps/client/src/pages/_app.tsx
+++ apps/client/src/pages/_app.tsx
@@ -3,6 +3,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import { JSX } from "react";
 import { Toaster } from "@kokomen/ui/components/toast/toaster";
+import { ErrorBoundary } from "@sentry/nextjs";
+import Error from "@/pages/_error";
 
 const queryClient: QueryClient = new QueryClient();
 
@@ -10,9 +12,13 @@ const queryClient: QueryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <QueryClientProvider client={queryClient}>
-      <Toaster>
-        <Component {...pageProps} />
-      </Toaster>
+      <ErrorBoundary
+        fallback={<Error statusCode={500} hasGetInitialProps={false} />}
+      >
+        <Toaster>
+          <Component {...pageProps} />
+        </Toaster>
+      </ErrorBoundary>
     </QueryClientProvider>
   );
 }
