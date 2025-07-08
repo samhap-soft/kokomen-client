eb27e3e3fd999b01ac6c9eff173b1366b7265afd
Minhyung Cho <dup33333@hufs.ac.kr>
2025-07-08 22:22:08 +0900

    feat: 회원 면접 API 함수 추가 및  객체 매핑 후 리턴하도록 변경

--- apps/client/src/domains/members/api/index.ts
+++ apps/client/src/domains/members/api/index.ts
@@ -1,16 +1,33 @@
-import { Rank } from "@/domains/members/types";
+import {
+  MemberInterview,
+  Rank,
+  TMemberInterviewResponse,
+} from "@/domains/members/types";
 import axios, { AxiosInstance } from "axios";
 
 const memberInstance: AxiosInstance = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}/members`,
+  baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}`,
   withCredentials: true,
 });
 
 const getRankList = async (page = 0, size = 10): Promise<Rank[]> => {
-  const response = await memberInstance.get("/ranking", {
+  const response = await memberInstance.get("/members/ranking", {
     params: { page, size },
   });
   return response.data;
 };
 
-export { getRankList };
+const getMemberInterviews = async (
+  memberId: number,
+  page = 0,
+  sort = "desc",
+  size = 10
+): Promise<MemberInterview[]> =>
+  memberInstance
+    .get<TMemberInterviewResponse[]>("/interviews", {
+      params: { member_id: memberId, page, size, sort: `id,${sort}` },
+    })
+    .then((res) => res.data)
+    .then((data) => data.map((interview) => new MemberInterview(interview)));
+
+export { getRankList, getMemberInterviews };
