937551f19c9d19c1526df91f515df6a05cd85a0a
Minhyung Cho <dup33333@hufs.ac.kr>
2025-07-08 22:21:07 +0900

    feat: 회원 면접 관련 타입 추가 및 객체 클래스 정의(필드명 카멜케이스로 변경 및 접근자 프로퍼티)

--- apps/client/src/domains/members/types/index.ts
+++ apps/client/src/domains/members/types/index.ts
@@ -5,4 +5,53 @@ type Rank = {
   finished_interview_count: number;
 };
 
-export type { Rank };
+type TMemberInterviewResponse = {
+  interview_id: number;
+  interview_state: "FINISHED";
+  interview_category: string;
+  created_at: string;
+  root_question: string;
+  max_question_count: number;
+  score: number;
+};
+
+type TMemberInterview = {
+  interviewId: number;
+  interviewState: "FINISHED";
+  interviewCategory: string;
+  createdAt: string;
+  rootQuestion: string;
+  maxQuestionCount: number;
+  score: number;
+};
+
+class MemberInterview implements TMemberInterview {
+  public interviewId: number;
+  public interviewState: "FINISHED";
+  public interviewCategory: string;
+  public createdAt: string;
+  public rootQuestion: string;
+  public maxQuestionCount: number;
+  public score: number;
+
+  public constructor(interview: TMemberInterviewResponse) {
+    this.interviewId = interview.interview_id;
+    this.interviewState = interview.interview_state;
+    this.interviewCategory = interview.interview_category;
+    this.createdAt = interview.created_at;
+    this.rootQuestion = interview.root_question;
+    this.maxQuestionCount = interview.max_question_count;
+    this.score = interview.score;
+  }
+
+  public get formattedCreatedDate(): string {
+    return new Date(this.createdAt).toLocaleDateString("ko-KR", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  }
+}
+
+export type { Rank, TMemberInterview, TMemberInterviewResponse };
+export { MemberInterview };
