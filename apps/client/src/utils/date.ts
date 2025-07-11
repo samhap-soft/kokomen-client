ba0996a06e46b7415ecf044ddcd9ff8be1118814
Minhyung Cho <dup33333@hufs.ac.kr>
2025-07-11 17:09:35 +0900

    fix: 날짜 형식에서 월 표시를 2자리에서 전체 이름으로 변경

--- apps/client/src/utils/date.ts
+++ apps/client/src/utils/date.ts
@@ -1,7 +1,7 @@
 const formatDate = (date: string) => {
   return new Date(date).toLocaleDateString("ko-KR", {
     year: "numeric",
-    month: "2-digit",
+    month: "long",
     day: "numeric",
   });
 };
