0e2480813a5fb92aee6790500dc2cd162b7e42da
Minhyung Cho <dup33333@hufs.ac.kr>
2025-07-11 15:01:30 +0900

    refactor: 면접 관련 타입 정의를 정리하고, 인터뷰 요약 및 결과 타입을 개선

--- apps/client/src/domains/members/types/index.ts
+++ apps/client/src/domains/members/types/index.ts
@@ -1,3 +1,28 @@
+type Rank = {
+  id: number;
+  nickname: string;
+  score: number;
+  finished_interview_count: number;
+};
+
+type InterviewSummary = {
+  interview_id: number;
+  interview_category: string;
+  created_at: string;
+  root_question: string;
+  max_question_count: number;
+  score: 20;
+  interview_like_count: number;
+  interview_already_liked: boolean;
+};
+
+type MemberInterview = {
+  interview_summaries: InterviewSummary[];
+  total_member_count: number;
+  interviewee_rank: number;
+  interviewee_nickname: string;
+};
+
 type Feedback = {
   question_id: number;
   answer_id: number;
@@ -9,113 +34,15 @@ type Feedback = {
   answer_already_liked: boolean;
 };
 
-type Rank = {
-  id: number;
-  nickname: string;
-  score: number;
-  finished_interview_count: number;
-};
-
-type TMemberInterviewResponse = {
-  interview_id: number;
-  interview_state: "FINISHED";
-  interview_category: string;
-  created_at: string;
-  root_question: string;
-  max_question_count: number;
-  score: number;
-};
-
-type TMemberInterview = {
-  interviewId: number;
-  interviewState: "FINISHED";
-  interviewCategory: string;
-  createdAt: string;
-  rootQuestion: string;
-  maxQuestionCount: number;
-  score: number;
-};
-
-class MemberInterview implements TMemberInterview {
-  public interviewId: number;
-  public interviewState: "FINISHED";
-  public interviewCategory: string;
-  public createdAt: string;
-  public rootQuestion: string;
-  public maxQuestionCount: number;
-  public score: number;
-
-  public constructor(interview: TMemberInterviewResponse) {
-    this.interviewId = interview.interview_id;
-    this.interviewState = interview.interview_state;
-    this.interviewCategory = interview.interview_category;
-    this.createdAt = interview.created_at;
-    this.rootQuestion = interview.root_question;
-    this.maxQuestionCount = interview.max_question_count;
-    this.score = interview.score;
-  }
-
-  public get formattedCreatedDate(): string {
-    return new Date(this.createdAt).toLocaleDateString("ko-KR", {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    });
-  }
-}
-
-type TMemberInterviewResultResponse = {
+type MemberInterviewResult = {
   feedbacks: Feedback[];
   total_feedback: string;
   total_score: number;
   interview_like_count: number;
   interview_already_liked: boolean;
+  interviewee_rank: number;
+  total_member_count: number;
+  interviewee_nickname: string;
 };
 
-type TMemberInterviewResult = {
-  feedbacks: {
-    questionId: number;
-    answerId: number;
-    question: string;
-    answer: string;
-    answerRank: string;
-    answerFeedback: string;
-    answerLikeCount: number;
-    answerLiked: boolean;
-  }[];
-  totalFeedback: string;
-  totalScore: number;
-  interviewLikeCount: number;
-  interviewAlreadyLiked: boolean;
-};
-
-const mapToMemberInterviewResult = (
-  response: TMemberInterviewResultResponse
-): TMemberInterviewResult => {
-  return {
-    feedbacks: response.feedbacks.map((feedback) => ({
-      questionId: feedback.question_id,
-      answerId: feedback.answer_id,
-      question: feedback.question,
-      answer: feedback.answer,
-      answerRank: feedback.answer_rank,
-      answerFeedback: feedback.answer_feedback,
-      answerLikeCount: feedback.answer_like_count,
-      answerLiked: feedback.answer_already_liked,
-    })),
-    totalFeedback: response.total_feedback,
-    totalScore: response.total_score,
-    interviewLikeCount: response.interview_like_count,
-    interviewAlreadyLiked: response.interview_already_liked,
-  };
-};
-
-export type {
-  Feedback,
-  Rank,
-  TMemberInterview,
-  TMemberInterviewResponse,
-  TMemberInterviewResult,
-  TMemberInterviewResultResponse,
-};
-export { MemberInterview, mapToMemberInterviewResult };
+export type { Feedback, Rank, MemberInterview, MemberInterviewResult };
