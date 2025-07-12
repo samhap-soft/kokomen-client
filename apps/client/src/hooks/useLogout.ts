11cc948b8ab5a34862e7ebafb5e455ddb3d6b937
Minhyung Cho <dup33333@hufs.ac.kr>
2025-07-12 17:36:18 +0900

    feat: 로그아웃 시 버튼 클릭 이벤트 캡처 기능 추가

--- apps/client/src/hooks/useLogout.ts
+++ apps/client/src/hooks/useLogout.ts
@@ -1,10 +1,16 @@
 import { logout } from "@/domains/auth/api";
+import { captureButtonEvent } from "@/utils/analytics";
 import { useMutation } from "@tanstack/react-query";
 import { useCallback } from "react";
 
 export const useLogout = () => {
   const logoutMutation = useMutation({
     mutationFn: logout,
+    onMutate: () => {
+      captureButtonEvent({
+        name: "userLogout",
+      });
+    },
     onSuccess: () => {
       window.location.href = "/";
     },
