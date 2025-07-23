65087894f4cf52f9336aac8abd6f66ce5604f45c
Minhyung Cho <dup33333@hufs.ac.kr>
2025-07-23 15:54:39 +0900

    feat : 웹뷰를 위한 리액트 프로젝트 기본적인 환경 설정

---
+++ apps/kokomen-webview/.eslintrc.cjs
@@ -0,0 +1,24 @@
+require("@kokomen/eslint-config/patch");
+
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  extends: ["@kokomen/eslint-config", "@kokomen/eslint-config/mixins/react"],
+  root: true,
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  settings: {
+    react: {
+      version: "19.0",
+    },
+  },
+
+  rules: {
+    "@typescript-eslint/no-floating-promises": "off",
+  },
+};
