fa9d0fa6701d7cc8dc592ac9f4af22b04e0ea64e
Minhyung Cho <dup33333@hufs.ac.kr>
2025-07-25 18:23:14 +0900

    feat : root에 이전 페이지로 갈 수 있는 버튼 추가

--- apps/kokomen-webview/src/routes/__root.tsx
+++ apps/kokomen-webview/src/routes/__root.tsx
@@ -1,15 +1,37 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import {
+  createRootRoute,
+  Outlet,
+  RootRoute,
+  useCanGoBack,
+  useRouter
+} from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { Button } from "@kokomen/ui/components/button";
+import { ChevronLeft } from "lucide-react";
 
-export const Route = createRootRoute({
-  component: () => {
-    return (
-      <>
-        <div className="p-2 flex gap-2"></div>
-        <hr />
-        <Outlet />
-        <TanStackRouterDevtools />
-      </>
-    );
-  }
+export const Route: RootRoute = createRootRoute({
+  component: RootComponent
 });
+
+function RootComponent() {
+  const canGoBack = useCanGoBack();
+  const router = useRouter();
+
+  return (
+    <>
+      <div className="p-2 flex gap-2 bg-primary ">
+        {!canGoBack && (
+          <Button
+            variant={"text"}
+            onClick={() => router.history.back()}
+            className="[&_svg]:size-6"
+          >
+            <ChevronLeft />
+          </Button>
+        )}
+      </div>
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  );
+}
