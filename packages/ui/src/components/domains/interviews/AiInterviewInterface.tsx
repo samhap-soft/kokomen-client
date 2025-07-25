cb8d76059613c142dc69aac820f04cbb6975f62c
Minhyung Cho <dup33333@hufs.ac.kr>
2025-07-25 18:22:00 +0900

    feat : 인터뷰 인터페이스를 위한 컴포넌트 추가

---
+++ packages/ui/src/components/domains/interviews/AiInterviewInterface.tsx
@@ -0,0 +1,38 @@
+import { InterviewerProps } from "@components/domains/interviews/avatarMesh";
+import { Interviewer } from "@components/domains/interviews/interviewer";
+import { Environment, Html } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { JSX, lazy, Suspense } from "react";
+
+// eslint-disable-next-line @rushstack/typedef-var
+const InterviewBackground = lazy(
+  () => import("#components/domains/interviews/interviewBackground.tsx")
+);
+
+export default function AiInterviewInterface({
+  emotion,
+  isListening,
+  isSpeaking
+}: InterviewerProps): JSX.Element {
+  return (
+    <Canvas camera={{ position: [0, 0, 2], fov: 40 }} shadows dpr={[1, 2]}>
+      <Suspense
+        fallback={
+          <Html fullscreen>
+            <div className="w-full h-full flex items-center justify-center text-xl font-bold text-nowrap bg-gradient-to-r from-blue-50 to-primary-bg-hover bg-opacity-80">
+              면접관님에게 전화하는 중...
+            </div>
+          </Html>
+        }
+      >
+        <InterviewBackground />
+        <Environment preset="lobby" resolution={2048} />
+        <Interviewer
+          emotion={emotion}
+          isSpeaking={isSpeaking}
+          isListening={isListening}
+        />
+      </Suspense>
+    </Canvas>
+  );
+}
---
+++ packages/ui/src/components/domains/interviews/interviewer.tsx
@@ -0,0 +1,35 @@
+import React, { JSX, lazy, memo, Suspense } from "react";
+import type { InterviewerProps } from "./avatarMesh";
+import { Html } from "@react-three/drei";
+
+// eslint-disable-next-line @rushstack/typedef-var
+const AvatarMesh = lazy(() => import("./avatarMesh"));
+
+export const Interviewer: React.FC<InterviewerProps> = memo(
+  ({
+    isSpeaking = false,
+    isListening = false,
+    emotion = "happy"
+  }: InterviewerProps): JSX.Element | null => {
+    return (
+      <Suspense
+        fallback={
+          <Html fullscreen>
+            <div className="w-full h-full flex items-center justify-center text-2xl font-bold text-nowrap text-text-light-solid bg-opacity-80">
+              면접관님이 허겁지겁 달려오는중..
+            </div>
+          </Html>
+        }
+      >
+        <group position={[0, -1.8, 1]}>
+          <AvatarMesh
+            isSpeaking={isSpeaking}
+            isListening={isListening}
+            emotion={emotion}
+          />
+        </group>
+      </Suspense>
+    );
+  }
+);
+Interviewer.displayName = "Interviewer";
