a4b306f9a53240732cb01149f3d8f9bdf2ded9c9
Minhyung Cho <dup33333@hufs.ac.kr>
2025-07-26 22:42:02 +0900

    dependency : path resolve를 위한 path 모듈 의존성 추가

--- apps/kokomen-webview/vite.config.js
+++ apps/kokomen-webview/vite.config.js
@@ -3,24 +3,40 @@ import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigpaths from "vite-tsconfig-paths";
+import { readFileSync } from "fs";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tanstackRouter({
       target: "react",
-      autoCodeSplitting: true,
+      autoCodeSplitting: true
     }),
     tailwindcss(),
     tsconfigpaths({
-      root: ".",
+      root: "."
     }),
-    react(),
+    react()
   ],
 
   server: {
     port: 3000,
     host: "127.0.0.1",
     allowedHosts: ["local.kokomen.kr"],
+    https: {
+      key: readFileSync("./local.kokomen.kr+2-key.pem"),
+      cert: readFileSync("./local.kokomen.kr+2.pem")
+    }
   },
+  assetsInclude: ["**/*.glb"],
+  resolve: {
+    alias: {
+      "@components": path.resolve(
+        __dirname,
+        "../../packages/ui/src/components"
+      ),
+      "@assets": path.resolve(__dirname, "../../packages/ui/src/assets")
+    }
+  }
 });
