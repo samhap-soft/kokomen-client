4eb7ede3838486a2a3eb4b591ca035c7902d0531
Minhyung Cho <dup33333@hufs.ac.kr>
2025-07-29 16:03:12 +0900

    feat: 로딩 원형 컴포넌트에 접근성 향상을 위한 role 속성 추가

--- packages/ui/src/components/spinner/index.tsx
+++ packages/ui/src/components/spinner/index.tsx
@@ -25,7 +25,7 @@ function RoundSpinner() {
 }
 function LoadingCircles() {
   return (
-    <div className="flex items-center justify-center space-x-2">
+    <div className="flex items-center justify-center space-x-2" role="status">
       <div className="w-4 h-4 rounded-full bg-primary-border animate-bounce [animation-delay:0ms]"></div>
       <div className="w-4 h-4 rounded-full bg-primary-hover animate-bounce [animation-delay:200ms]"></div>
       <div className="w-4 h-4 rounded-full bg-primary animate-bounce [animation-delay:400ms]"></div>
