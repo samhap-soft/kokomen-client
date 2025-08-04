441132fabf70ffc3f30bacc19cd805e3f2c4699e
Minhyung Cho <dup33333@hufs.ac.kr>
2025-08-04 15:10:53 +0900

    feat: 인터뷰 메인 화면에 음성 인식 기능 추가 및 웹뷰 메시지 처리 로직 구현

--- apps/kokomen-native/src/screens/interviews/interviewMain.tsx
+++ apps/kokomen-native/src/screens/interviews/interviewMain.tsx
@@ -1,11 +1,12 @@
-import { useEffect, useRef } from "react";
+import useSpeechRecognition from "@/hooks/useSpeechRecognition";
+import { useRef } from "react";
 import {
   KeyboardAvoidingView,
   Platform,
   SafeAreaView,
   View,
 } from "react-native";
-import WebView from "react-native-webview";
+import WebView, { WebViewMessageEvent } from "react-native-webview";
 
 export default function InterviewMainScreen() {
   const webviewRef = useRef<WebView>(null);
@@ -13,6 +14,27 @@ export default function InterviewMainScreen() {
       window.isNativeApp = true;
       true;
     `;
+
+  const { handleStart, handleStop } = useSpeechRecognition({
+    onResult: (transcript) => {
+      webviewRef.current?.postMessage(
+        JSON.stringify({
+          type: "speechRecognitionResult",
+          data: transcript,
+        }),
+      );
+    },
+  });
+  const handleMessage = (event: WebViewMessageEvent) => {
+    const data = JSON.parse(event.nativeEvent.data);
+    if (data.type === "startListening") {
+      console.log("startListening");
+      handleStart();
+    } else if (data.type === "stopListening") {
+      console.log("stopListening");
+      handleStop();
+    }
+  };
 
   return (
     <View style={{ flex: 1, backgroundColor: "#fff" }}>
@@ -32,6 +54,7 @@ export default function InterviewMainScreen() {
             originWhitelist={["*"]}
             injectedJavaScriptBeforeContentLoaded={runFirst}
             webviewDebuggingEnabled
+            onMessage={handleMessage}
             style={{ flex: 1 }}
             setBuiltInZoomControls={false}
             domStorageEnabled={true}
