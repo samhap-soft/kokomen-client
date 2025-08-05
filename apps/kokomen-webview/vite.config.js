f6fc9e4c1fd9a74eb70d63ddd972633f9a3387e6
Minhyung Cho <dup33333@hufs.ac.kr>
2025-08-05 14:19:22 +0900

    refactor: Vite 설정을 함수형으로 변경하여 개발 모드에서의 서버 실행 시에만 인증서 검사하도록 변경

--- apps/kokomen-webview/vite.config.js
+++ apps/kokomen-webview/vite.config.js
@@ -6,36 +6,41 @@ import tsconfigpaths from "vite-tsconfig-paths";
 import { readFileSync } from "fs";
 import path from "path";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    tanstackRouter({
-      target: "react",
-      autoCodeSplitting: true
+export default defineConfig(({ command, mode }) => {
+  const isDev = command === "serve";
+
+  return {
+    plugins: [
+      tanstackRouter({
+        target: "react",
+        autoCodeSplitting: true
+      }),
+      tailwindcss(),
+      tsconfigpaths({
+        root: "."
+      }),
+      react()
+    ],
+    optimizeDeps: {
+      include: ["@kokomen/ui"]
+    },
+    base: "./",
+    ...(isDev && {
+      server: {
+        port: 3000,
+        host: "127.0.0.1",
+        allowedHosts: ["local.kokomen.kr"],
+        https: {
+          key: readFileSync("./local.kokomen.kr+2-key.pem"),
+          cert: readFileSync("./local.kokomen.kr+2.pem")
+        }
+      }
     }),
-    tailwindcss(),
-    tsconfigpaths({
-      root: "."
-    }),
-    react()
-  ],
-  optimizeDeps: {
-    include: ["@kokomen/ui"]
-  },
-  base: "./",
-  server: {
-    port: 3000,
-    host: "127.0.0.1",
-    allowedHosts: ["local.kokomen.kr"],
-    https: {
-      key: readFileSync("./local.kokomen.kr+2-key.pem"),
-      cert: readFileSync("./local.kokomen.kr+2.pem")
+    assetsInclude: ["**/*.glb"],
+    resolve: {
+      alias: {
+        "@assets": path.resolve(__dirname, "../../packages/ui/src/assets")
+      }
     }
-  },
-  assetsInclude: ["**/*.glb"],
-  resolve: {
-    alias: {
-      "@assets": path.resolve(__dirname, "../../packages/ui/src/assets")
-    }
-  }
+  };
 });
