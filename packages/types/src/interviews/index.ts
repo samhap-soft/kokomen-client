12c71e6b6b2302e12ab792c6e94d96ddebe7076f
Minhyung Cho <dup33333@hufs.ac.kr>
2025-08-14 16:25:08 +0900

    feat: 인터뷰 API 관련 타입 정의를 type 패키지에 추가 및 구조 개선

--- packages/types/src/interviews/index.ts
+++ packages/types/src/interviews/index.ts
@@ -1,6 +1,7 @@
 import { Dispatch } from "react";
 
 type InterviewStatus = "standby" | "thinking" | "question" | "finished";
+type AnswerScore = "A" | "B" | "C" | "D" | "F";
 
 type QuestionAndAnswer = {
   question_id: number;
@@ -10,22 +11,30 @@ type QuestionAndAnswer = {
 };
 type PrevQuestionAndAnswers = Array<QuestionAndAnswer>;
 
-type Interview = {
+type BaseInterview = {
   interview_id: number;
   interview_state: "IN_PROGRESS" | "FINISHED";
   prev_questions_and_answers: PrevQuestionAndAnswers;
   cur_question_id: number;
   cur_question_count: number;
-  cur_question: string;
   max_question_count: number;
 };
+
+type TextInterview = BaseInterview & {
+  cur_question: string;
+};
+
+type VoiceInterview = BaseInterview & {
+  cur_question_voice_url: string;
+};
+type InterviewMode = "TEXT" | "VOICE";
+type Interview = TextInterview | VoiceInterview;
 
 type InterviewConfig = {
   category: string;
   max_question_count: number;
-  interview_type: InterviewType;
+  interview_type: InterviewMode;
 };
-type InterviewType = "TEXT" | "VOICE";
 interface InterviewState {
   message: string;
   status: InterviewStatus;
@@ -33,6 +42,63 @@ interface InterviewState {
   questionsAndAnswers: Omit<QuestionAndAnswer, "answer_id" | "question_id">[];
 }
 
+type InterviewAnswerForm = {
+  interviewId: number;
+  questionId: number;
+  answer: string;
+  mode: InterviewMode;
+};
+
+type ProceedState =
+  | "LLM_PENDING"
+  | "LLM_FAILED"
+  | "TTS_PENDING"
+  | "TTS_FAILED"
+  | "COMPLETED";
+
+// Polling중 기본적으로 가지고 있는 필드
+type BaseTextInterviewSubmitStatus = {
+  proceed_state: ProceedState;
+};
+
+// LLM과 TTS 모두 성공한 경우
+type InterviewSubmitPollingSuccess = BaseTextInterviewSubmitStatus & {
+  proceed_state: ProceedState;
+  interview_state: "FINISHED";
+  cur_answer_rank: AnswerScore;
+  next_question_id: number;
+  next_question_voice_url?: string;
+  next_question?: string;
+};
+
+// LLM 폴링 중
+type InterviewSubmitPollingLLMPending = BaseTextInterviewSubmitStatus & {
+  proceed_state: "LLM_PENDING";
+};
+
+// LLM 폴링 중 실패한 경우
+type InterviewSubmitPollingLLMFailed = BaseTextInterviewSubmitStatus & {
+  proceed_state: "LLM_FAILED";
+};
+
+// TTS 폴링 중
+type InterviewSubmitPollingTTSPending = BaseTextInterviewSubmitStatus & {
+  proceed_state: "TTS_PENDING";
+};
+
+// TTS 폴링 중 실패한 경우
+type InterviewSubmitPollingTTSFailed = BaseTextInterviewSubmitStatus & {
+  proceed_state: "TTS_FAILED";
+};
+
+type InterviewSubmitPolling =
+  | InterviewSubmitPollingSuccess
+  | InterviewSubmitPollingLLMPending
+  | InterviewSubmitPollingLLMFailed
+  | InterviewSubmitPollingTTSPending
+  | InterviewSubmitPollingTTSFailed;
+
+// Interview 관련 reducer 액션 타입
 type InterviewActions = Dispatch<InterviewAction>;
 
 interface StartupAction {
@@ -73,7 +139,12 @@ export type {
   PrevQuestionAndAnswers,
   Interview,
   InterviewConfig,
-  InterviewType,
   InterviewState,
-  InterviewActions
+  InterviewActions,
+  InterviewMode,
+  InterviewSubmitPolling,
+  InterviewSubmitPollingSuccess,
+  InterviewAnswerForm,
+  ProceedState,
+  AnswerScore
 };
