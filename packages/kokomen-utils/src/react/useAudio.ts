994608af40e36a22e88c8527b02cfa0de464038e
Minhyung Cho <dup33333@hufs.ac.kr>
2025-08-15 20:50:51 +0900

    reafctor : autoplay로 인한 자동 재생이 ended 이벤트를 dispatch하지 못한 문제를 해결하고자 src 교체 후 Loadeddata 이벤트에 재생할 수 있도록 개선

--- packages/kokomen-utils/src/react/useAudio.ts
+++ packages/kokomen-utils/src/react/useAudio.ts
@@ -1,16 +1,19 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, useCallback } from "react";
 
 interface UseAudioOptions {
   onPlayStart?: () => void;
   onPlayEnd?: () => void;
   onPlayError?: () => void;
 }
+
 function useAudio(
   audioUrl: string,
   { onPlayStart, onPlayEnd, onPlayError }: UseAudioOptions
 ) {
   const audioRef = useRef<HTMLAudioElement | null>(null);
   const [playFinished, setPlayFinished] = useState<boolean>(false);
+  const loadingRef = useRef<boolean>(false);
+  const readyRef = useRef<boolean>(false);
 
   // audio 요소를 동적으로 생성하고 관리
   useEffect(() => {
@@ -22,7 +25,14 @@ function useAudio(
     // 새로운 audio 요소 생성
     const audio = document.createElement("audio");
     audio.preload = "auto";
+    audio.src = audioUrl;
     audioRef.current = audio;
+
+    const handleError = () => {
+      loadingRef.current = false;
+      readyRef.current = false;
+      onPlayError?.();
+    };
 
     // 이벤트 리스너 추가
     const handleEnded = () => {
@@ -30,56 +40,81 @@ function useAudio(
       onPlayEnd?.();
     };
 
-    const handleError = () => {
-      onPlayError?.();
+    const handleCanPlayThrough = () => {
+      loadingRef.current = false;
+      readyRef.current = true;
+    };
+
+    const handleLoadStart = () => {
+      loadingRef.current = true;
+      readyRef.current = false;
+    };
+
+    const handlePlay = () => {
+      setPlayFinished(false);
+      onPlayStart?.();
     };
 
     audio.addEventListener("ended", handleEnded);
     audio.addEventListener("error", handleError);
+    audio.addEventListener("canplaythrough", handleCanPlayThrough);
+    audio.addEventListener("loadstart", handleLoadStart);
+    audio.addEventListener("play", handlePlay);
 
     // cleanup 함수
     return () => {
       audio.removeEventListener("ended", handleEnded);
       audio.removeEventListener("error", handleError);
+      audio.removeEventListener("canplaythrough", handleCanPlayThrough);
+      audio.removeEventListener("loadstart", handleLoadStart);
+      audio.removeEventListener("play", handlePlay);
       audio.remove();
       audioRef.current = null;
     };
   }, []);
 
-  const playAudio = (): Promise<void> => {
-    if (audioRef.current) {
-      setPlayFinished(false);
-      onPlayStart?.();
-      return audioRef.current.play();
-    }
-    return Promise.reject(new Error("Audio element not available"));
-  };
+  const playAudio = useCallback(
+    async (audioUrl?: string) => {
+      if (!audioRef.current) {
+        throw new Error("Audio element not available");
+      }
+      if (!audioUrl) {
+        return audioRef.current.play();
+      }
 
-  const stopAudio = (): void => {
+      audioRef.current.src = audioUrl;
+      audioRef.current.addEventListener("loadeddata", () => {
+        loadingRef.current = false;
+        readyRef.current = true;
+        audioRef.current.play().catch((err) => {
+          console.error(err);
+        });
+      });
+    },
+    [onPlayStart]
+  );
+
+  const stopAudio = useCallback((): void => {
     if (audioRef.current) {
       audioRef.current.pause();
     }
-  };
+  }, []);
 
-  const resetAudio = (): void => {
+  const resetAudio = useCallback((): void => {
     if (audioRef.current) {
       audioRef.current.currentTime = 0;
     }
-  };
-
-  // audioUrl이 변경될 때마다 src 업데이트
-  useEffect(() => {
-    if (!audioUrl || !audioRef.current) return;
-    audioRef.current.src = audioUrl;
-    audioRef.current.load();
-  }, [audioUrl]);
+  }, []);
 
   return {
     playAudio,
     stopAudio,
     resetAudio,
     audioRef,
-    playFinished
+    playFinished,
+    loadingRef,
+    readyRef
   };
 }
+
 export { useAudio };
