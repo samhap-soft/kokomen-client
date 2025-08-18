ac02f4bc463d33002936304616904e144c9bb348
Minhyung Cho <dup33333@hufs.ac.kr>
2025-08-18 18:06:21 +0900

    refactor: 인터뷰 컴포넌트의 로딩 상태에서 Html 컴포넌트의 zIndexRange 속성을 추가하여 모달을 덮지 않도록 개선

--- packages/ui/src/components/domains/interviews/AiInterviewInterface.tsx
+++ packages/ui/src/components/domains/interviews/AiInterviewInterface.tsx
@@ -5,9 +5,7 @@ import { Canvas } from "@react-three/fiber";
 import { JSX, lazy, Suspense } from "react";
 
 // eslint-disable-next-line @rushstack/typedef-var
-const InterviewBackground = lazy(
-  () => import("./interviewBackground")
-);
+const InterviewBackground = lazy(() => import("./interviewBackground"));
 
 export default function AiInterviewInterface({
   emotion,
@@ -18,7 +16,7 @@ export default function AiInterviewInterface({
     <Canvas camera={{ position: [0, 0, 2], fov: 40 }} shadows dpr={[1, 2]}>
       <Suspense
         fallback={
-          <Html fullscreen>
+          <Html fullscreen zIndexRange={[40, 49]}>
             <div className="w-full h-full flex items-center justify-center text-xl font-bold text-nowrap bg-gradient-to-r from-blue-50 to-primary-bg-hover bg-opacity-80">
               면접관님에게 전화하는 중...
             </div>
