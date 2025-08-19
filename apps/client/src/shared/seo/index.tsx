f0d122f7605c5a0fc68a9cee174e684dbed28aa3
Minhyung Cho <dup33333@hufs.ac.kr>
2025-08-19 21:14:45 +0900

    feat: 새로운 아이콘 및 webmanifest 파일 추가

--- apps/client/src/shared/seo/index.tsx
+++ apps/client/src/shared/seo/index.tsx
@@ -23,7 +23,7 @@ export const SEO = ({
   image,
   pathname,
   children,
-  robots,
+  robots
 }: SEOprops): JSX.Element => {
   return (
     <Head>
@@ -55,6 +55,28 @@ export const SEO = ({
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="robots" content={robots ?? "noindex, nofollow"} />
       <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <meta
+        name="google-site-verification"
+        content="mexmU538xwEq2CneNjUio6wcbAOL5Z3Ld0IyDaoR-9s"
+      />
 
       {children}
     </Head>
