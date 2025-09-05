1af2bb56ee7a14a73f665bdec2ceed9bf5a92af2
Minhyung Cho <dup33333@hufs.ac.kr>
2025-09-05 22:10:35 +0900

    feat: 대시보드에 스트릭 컴포넌트 및 스토리북 추가

---
+++ apps/client/src/domains/dashboard/components/streak.tsx
@@ -0,0 +1,45 @@
+import { useQuery } from "@tanstack/react-query";
+import { meKeys } from "@kokomen/utils";
+import { getStreak } from "@/domains/dashboard/api";
+import { useState } from "react";
+import { StreakCalendar } from "@kokomen/ui/domains";
+import { Skeleton } from "@kokomen/ui";
+
+export default function Streak() {
+  const startDate = new Date(new Date().setDate(new Date().getDate() - 365))
+    .toISOString()
+    .split("T")[0];
+  const endDate = new Date().toISOString().split("T")[0];
+  const [streakDate, setStreakDate] = useState<{
+    startDate: string;
+    endDate: string;
+  }>({
+    startDate: startDate,
+    endDate: endDate
+  });
+  const {
+    data: streakData,
+    isLoading,
+    isError
+  } = useQuery({
+    queryKey: [meKeys.streak()],
+    queryFn: () => getStreak(streakDate.startDate, streakDate.endDate),
+    staleTime: 1000 * 60 * 60 * 24,
+    gcTime: 1000 * 60 * 60 * 24,
+    retry: 1
+  });
+  console.log(streakData);
+  if (isLoading) return <Skeleton className="w-full h-44" />;
+  if (isError || !streakData) return null;
+
+  return (
+    <div className="max-w-[850px] mx-auto">
+      <StreakCalendar
+        className="mx-auto"
+        streak={streakData}
+        startDate={streakDate.startDate}
+        endDate={streakDate.endDate}
+      />
+    </div>
+  );
+}
