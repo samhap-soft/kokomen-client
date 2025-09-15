15ab1774e4b42769877235bad71b213039b52a1f
Minhyung Cho <dup33333@hufs.ac.kr>
2025-09-16 03:04:00 +0900

    feat: CORS 설정 추가 및 서버 포트 변경
    - CORS 설정을 추가하여 클라이언트 도메인 및 요청 메서드, 헤더를 정의
    - 서버가 3000 포트 대신 3001 포트에서 리스닝하도록 변경

--- apps/kokomen-server/src/main.ts
+++ apps/kokomen-server/src/main.ts
@@ -1,8 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // CORS 설정
+  app.enableCors({
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "http://local.kokomen.kr:3000",
+      "https://dev.kokomen.kr"
+    ], // 클라이언트 도메인
+    credentials: true, // 쿠키 포함 허용
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cookie"]
+  });
+
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
