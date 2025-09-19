483f64a3fa785656bbb48b9d470ffcb9ab2712a0
Minhyung Cho <dup33333@hufs.ac.kr>
2025-09-19 12:03:04 +0900

    feat: CategoryModule 및 RootQuestionModule 추가, CORS 설정 개선 및 DB 포트 기본값 설정

--- apps/kokomen-server/src/config/db.config.ts
+++ apps/kokomen-server/src/config/db.config.ts
@@ -3,7 +3,7 @@ import { registerAs } from "@nestjs/config";
 export default registerAs("db", () => ({
   type: "mysql",
   host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT, 10),
+  port: parseInt(process.env.DB_PORT || "3306", 10),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE
