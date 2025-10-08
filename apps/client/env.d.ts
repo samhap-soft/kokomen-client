702594822690e1806249dcd95916e2d837a8728d
Minhyung Cho <dup33333@hufs.ac.kr>
2025-10-08 22:14:32 +0900

    feat : bedrock 환경변수 및 api Route url 환경변수 추가

--- apps/client/env.d.ts
+++ apps/client/env.d.ts
@@ -5,8 +5,10 @@ declare const process: {
     NEXT_PUBLIC_POSTHOG_HOST: string;
     NEXT_PUBLIC_BASE_URL: string;
     NEXT_PUBLIC_NOTIFICATION_API_BASE_URL: string;
+    NEXT_PUBLIC_GRAPHQL_URL: string;
     NEXT_PUBLIC_API_BASE_URL: string;
     NEXT_PUBLIC_V2_API_BASE_URL: string;
+    NEXT_PUBLIC_V3_API_BASE_URL: string;
     NEXT_PUBLIC_CDN_BASE_URL: string;
     NEXT_PUBLIC_GOOGLE_CLIENT_ID: string;
   };
